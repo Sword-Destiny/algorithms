55659c13ce2e3997bcf43ab20680d469c768cf09
天命剑主 <yuanhonglong@outlook.com>
2018-06-07 10:35:17 +0800

    新增算法

---
+++ include/mid_level/blocks.h
@@ -0,0 +1,34 @@
+//
+// Created by yuanh on 2018/6/7.
+//
+
+#ifndef ALGORITHMS_BLOCKS_H
+
+#include "matrix.h"
+
+template<typename T>
+/**
+ * 染色问题,使用递推矩阵求解
+ * @tparam T 类型参数
+ * @param n n
+ * @param mod 模
+ * @return 方案个数
+ */
+T blocks(int n, T mod) {
+    matrix<T> m(3, 3);
+    m[0][0] = 2;
+    m[0][1] = 1;
+    m[0][2] = 0;
+    m[1][0] = 2;
+    m[1][1] = 2;
+    m[1][2] = 2;
+    m[2][0] = 0;
+    m[2][1] = 1;
+    m[2][2] = 2;
+    m = m.pow(n, mod);
+    return m[0][0];
+}
+
+#define ALGORITHMS_BLOCKS_H
+
+#endif //ALGORITHMS_BLOCKS_H
---
+++ src/mid_level/blocks.cpp
@@ -0,0 +1,18 @@
+//
+// Created by yuanh on 2018/6/7.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#include "../../include/mid_level/blocks.h"
+
+int main() {
+    int mod = 10007;
+    for (int i = 1; i <= 10; ++i) {
+        cout << "blocks " << i << " : " << blocks(i, mod) << endl;
+    }
+    return 0;
+}
