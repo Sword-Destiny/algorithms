f8594a84184472b12b5bdb0d9dbcee5f4fb15fce
天命剑主 <yuanhonglong@outlook.com>
2018-06-15 17:38:10 +0800

    新增算法

---
+++ include/mid_level/sqrt.h
@@ -0,0 +1,67 @@
+//
+// Created by yuanh on 2018/6/15.
+//
+
+#ifndef ALGORITHMS_SQRT_H
+
+#include <cmath>
+
+using std::abs;
+
+#ifndef ACCURACY //精度
+#define ACCURACY 0.000000001
+#endif
+
+/**
+ * f = θ^2 - x
+ * 令f = 0
+ * 则θ = sqrt(x)
+ * 可以用牛顿法逼近
+ * θ[i+1] = θ[i] - f(θ[i])/f'(θ[i])
+ * @param x 求x的平方根
+ * @return x的平方根
+ */
+double sqrt_newton(double x) {
+    if (x == 0) return 0;
+    double theta = 0.0f;
+    double theta2 = 1.0f;
+    while (abs(theta2 - theta) > ACCURACY) {
+        theta = theta2;
+        theta2 = theta - (theta * theta - x) / (2.0f * theta);
+    }
+    return theta2;
+}
+
+float sqrt_newton(float x) {
+    if (x == 0) return 0;
+    float theta = 0.0f;
+    float theta2 = 1.0f;
+    while (abs(theta2 - theta) > ACCURACY) {
+        theta = theta2;
+        theta2 = theta - (theta * theta - x) / (2.0f * theta);
+    }
+    return theta2;
+}
+
+/**
+ * 求x平方根
+ * 这个无法检测小于0的非法情况,参数小于0会返回0
+ * 约翰·卡马克
+ * @param x 求x平方根
+ * @return x平方根
+ */
+float sqrt_fast(float x) {
+    if (x == 0) return 0;
+    float result = x;
+    float xhalf = 0.5f * result;
+    int i = *(int *) &result;
+    i = 0x5f375a86 - (i >> 1);
+    result = *(float *) &i;
+    result = result * (1.5f - xhalf * result * result);
+    result = result * (1.5f - xhalf * result * result);
+    return 1.0f / result;
+}
+
+#define ALGORITHMS_SQRT_H
+
+#endif //ALGORITHMS_SQRT_H
--- src/base/hello_world.cpp
+++ src/base/hello_world.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <iomanip>
+#include "../../include/mid_level/sqrt.h"
 
 using std::cout;
 using std::endl;
+using std::setprecision;
+using std::setiosflags;
+using std::ios;
 
 int main() {
-    unsigned long a=1, b = 13;
+    unsigned long a = 1, b = 13;
     asm ("bsr %1,%%eax ;"
          "movl %%eax,%0 ;"
-         : "=row"(a)
-         : "row" (b)
+    : "=m"(a)
+    : "m" (b)
     );
-    cout << "hello world! " << (1<<(a+1)) << endl;
+    cout << "hello world! " << (1 << (a + 1)) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton(5000000000000.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton(0.0005f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton(0.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton(-1.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton((double)5000000000000.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton((double)0.0005f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton((double)0.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_newton((double)-1.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_fast(5000000000000.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_fast(0.0005f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_fast(0.0f) << endl;
+    cout << setiosflags(ios::fixed) << setprecision(64) << sqrt_fast(-1.0f) << endl;
     return 0;
 }
