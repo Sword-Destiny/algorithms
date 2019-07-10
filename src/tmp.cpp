9bf4588f3c8767ba2eb42de4cd31312def7ee3b6
天命剑主 <yuanhonglong@outlook.com>
2019-07-10 14:39:13 +0800

    新增算法

---
+++ src/tmp.cpp
@@ -0,0 +1,15 @@
+//
+// Created by yuanhao on 2019-7-10.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a, b;
+    while (cin >> a >> b) {
+        cout << (a + b) << endl;
+    }
+    return 0;
+}
