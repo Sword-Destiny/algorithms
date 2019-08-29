8d81af9efcc445bdc02a6996c2ad8afc977a32e6
天命剑主 <yuanhonglong@outlook.com>
2019-08-29 14:27:39 +0800

    修改数据类型

--- src/mid_level/fast_fibonacci.cpp
+++ src/mid_level/fast_fibonacci.cpp
@@ -9,9 +9,10 @@ using std::endl;
 #include "../../include/mid_level/fast_fibonacci.h"
 
 int main() {
-    long long mod = 0x7fffffffffffffff;
+    long long mod = 1e9 + 7;
     for (unsigned int i = 0; i < 50; ++i) {
         cout << "fast_fibonacci " << i << " : " << fast_fibonacci(i, mod) << endl;
     }
+    cout << fast_fibonacci(4593613583255041, mod) << endl;
     return 0;
 }
