9ca21d92af16e1200d4060ed66b9b38b7243fa8a
SwordDestiny <yuanhonglong@outlook.com>
2020-10-25 22:41:27 +0800

    ignore

--- src/tmp.cpp
+++ src/tmp.cpp
@@ -1,29 +1,49 @@
+#include <sys/mman.h>
 #include <iostream>
-#include <cmath>
+#include <unistd.h>
 
 using namespace std;
 
+static const size_t len = 8192;
+
 int main() {
-    int n;
-    cin >> n;
-    int size = pow(2, n);
-    int a[size];
-    int i = 1;
-    a[0] = -1;
-    int len = 1;
-    for (; i < n; i++) {
-        a[len] = -1;
-        for (int j = 1; j <= len; j++) {
-            a[len + j] = -a[len - j];
-        }
-        len = len * 2 + 1;
+
+    int *p = (int *) mmap(nullptr, len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    int *pp = (int *) mmap(((char *)p) + len, len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    printf("p %p pp %p\n", p, pp);
+
+    int size = len / sizeof(int);
+    // read without write
+    for (int i = 0; i < size; ++i) {
+        cout << p[i] << " ";
     }
-    for (int index = 0; index < len; index++) {
-        if (a[index] == -1) {
-            cout << "down" << endl;
-        } else {
-            cout << "up" << endl;
-        }
+    cout << endl;
+
+    // write
+    for (int i = 0; i < size; ++i) {
+        p[i] = i;
     }
+
+    // read first 2 page
+    for (int i = 0; i < size; ++i) {
+        cout << p[i] << " ";
+    }
+    cout << endl;
+
+    // unmap first page
+    (void) munmap(p, len / 2);
+
+    // read 2nd page
+    for (int i = size / 2; i < size; ++i) {
+        cout << p[i] << " ";
+    }
+    cout << endl;
+
+    // unmap 2nd page
+    (void) munmap(p + len / 2, len / 2);
+
+    int m;
+    cin >> m;
+
     return 0;
 }
